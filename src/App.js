bf59404fd4fe4bcaf589a48fad78a3680ec4d88c
zzzicky <zzzicky3@gmail.com>
2022-05-06 18:13:30 +0800

    首页divä¸

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
 import './App.css';
+import React from "react";
+import logo from './icons/logo.svg';
+import log from './images/log.png';
+import skip from './icons/skip.svg';
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const Table = styled.div`
+  width: 100%;
+  height: 100%;
+  
+  .navbar {
+    box-sizing: border-box;
+    border-bottom: 1px solid #eaecef;
+    width: 100%;
+    position: fixed;
+    padding: 10px 12px;
+    line-height: 32px;
+    z-index: 1;
+    a {
+      background: red;
+    }
+  }
+`
+
+
+const Home = () => {
+    return (
+        <Table>
+            <header className="navbar">
+                <a href="/reacts-ui/" style={{textDecoration: "none"}}>
+                    <img src={logo} alt=""/>
+                    <span style={{fontSize: "23px", fontWeight: 600, color: "#282c34"}}>Reacts-UI</span>
+                </a>
+                <div className="links">
+                    <div className="search"></div>
+                    <nav>
+                        <div>
+                            <a href="">主页</a>
+                        </div>
+                        <div>
+                            <a href="">
+                                文档
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                Github
+                            </a>
+                            <img src={skip} alt=""/>
+                        </div>
+                    </nav>
+                </div>
+            </header>
+            <aside>
+            </aside>
+            <main>
+                <header>
+                    <img src={log} alt=""/>
+                    <h1>Reacts-UI</h1>
+                    <p>基于React的UI组件库</p>
+                    <p className="action">
+                        <a href="/reacts-ui/get-started/" className="getStart">快速开始</a>
+                    </p>
+                </header>
+                <div className="features">
+
+                </div>
+                <footer>222</footer>
+            </main>
+
+        </Table>
+    )
 }
 
-export default App;
+export default Home;
