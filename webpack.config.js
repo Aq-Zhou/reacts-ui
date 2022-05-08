86ecf274bee88f387134f202b8c013a357087b2b
zzz <17704904@qq.com>
2022-05-08 21:15:33 +0800

    webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
+const { library } = require("webpack");
+
 module.exports = {
+    // mode:'production',
     entry: {
         index: './lib/index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname + 'dist/lib'),
+        library: 'Reacts-UI',
+        libraryTarget: 'umd',
     },
     modules: {
         rules: [
